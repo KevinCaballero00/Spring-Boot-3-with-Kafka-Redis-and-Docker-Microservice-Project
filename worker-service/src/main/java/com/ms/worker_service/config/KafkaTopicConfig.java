fdfc530ba78d0531a852e14625d8023275f3532e
KevinCaballero00 <kevinarturocc@ufps.edu.co>
2026-01-06 17:30:32 -0500

    Fourth commit

--- worker-service/src/main/java/com/ms/worker_service/config/KafkaTopicConfig.java
+++ worker-service/src/main/java/com/ms/worker_service/config/KafkaTopicConfig.java
@@ -1,11 +1,11 @@
-package com.ms.news.config;
+package com.ms.worker_service.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
-import static com.ms.news.utils.Contants.TOPIC_NAME;
+import static com.ms.worker_service.utils.Contants.TOPIC_NAME;
 
 @Configuration
 public class KafkaTopicConfig {
---
+++ worker-service/src/main/java/com/ms/worker_service/utils/Contants.java
@@ -0,0 +1,10 @@
+package com.ms.worker_service.utils;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class Contants {
+
+    public static final String TOPIC_NAME = "news";
+    public static final String MESSAGE_GROUP_NAME = "news";
+}
